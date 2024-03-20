c9e7dfd74b4454afd127fd3d4483e25beeac9f55
felipebpaimdecarvalho <160735500+felipebpaimdecarvalho@users.noreply.github.com>
2024-03-20 09:36:59 -0300

    Add files via upload

---
+++ exemplo.py
@@ -0,0 +1,51 @@
+print ("Felipe")
+
+numero = 10
+nome = "Felipe"
+booleano = True
+a = 10
+b = 5
+soma = a + b
+subtraçao = a - b
+multiplicaçao = a * b
+divisao = a / b
+
+
+num1 = 10
+num2 = 20
+num3 = 30
+num4 = 40
+
+
+frase = "Esta é uma frase de exemplo."
+
+
+palavra = "Python"
+
+
+media = (num1 + num2 + num3 + num4) / 4
+print("Média aritmética dos números:", media)
+
+
+quadrado_num = num1 ** 2
+print("Quadrado de num1:", quadrado_num)
+
+
+dobro_num = num2 * 2
+print("Dobro de num2:", dobro_num)
+
+
+quantidade_letras = len(palavra)
+print("Quantidade de letras na palavra:", quantidade_letras)
+
+
+quantidade_espacos = frase.count(" ")
+print("Quantidade de espaços em branco na frase:", quantidade_espacos)
+
+
+primeiro_maior = num1 > num2
+print("O primeiro número é maior que o segundo:", primeiro_maior)
+
+
+maior_numero = max(num1, num2, num3, num4)
+print("O maior número é:", maior_numero)
